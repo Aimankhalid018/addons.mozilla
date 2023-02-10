d9b6cdbfdd4fcbaae8f5f57b2938c0fca194dd01
Aimankhalid018 <124768770+Aimankhalid018@users.noreply.github.com>
2023-02-10 13:25:33 +0500

    Add files via upload

---
+++ webpython.py
@@ -0,0 +1,55 @@
+#Variable code
+x = 10
+y = "aiman"
+print(x)
+print(y)
+
+#String code
+x = "my name is aiman"
+print(x)
+
+#list code
+list = ["aiman", "khalid", "055"]
+print(list)
+
+#tuple code
+tuple = ("aiman", "khalid", "055")
+print(tuple)
+
+#set code
+set = {"aiman", "khalid", "055"}
+print(set)
+
+#dictionaries code
+dict = {
+  "name": "aiman khalid",
+  "roll no": 55
+}
+print(dict)
+
+#If-else code
+a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+#while loop code
+i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
+
+  #for loop code
+  fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+#function code
+def my_function():
+  print("my name is aiman")
+
+my_function()
